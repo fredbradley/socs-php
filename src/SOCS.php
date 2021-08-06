ef06297a666e1914e504b0e217463d4bfa9898c3
Fred Bradley <code@fredbradley.co.uk>
2021-08-06 20:58:01 +0100

    concept and test

--- src/SOCS.php
+++ src/SOCS.php
@@ -2,19 +2,64 @@
 
 namespace FredBradley\SOCS;
 
+use Dotenv\Dotenv;
+use GuzzleHttp\Client;
+
 class SOCS
 {
+    /**
+     * @var int
+     */
     public $socsId;
+    /**
+     * @var string
+     */
     public $apiKey;
 
-    public function __construct(int $socsId, string $apiKey)
+    /**
+     * @var Client
+     */
+    private $client;
+    public function __construct(string $socsId='SOCSID', string $apiKey='SOCSAPIKEY')
     {
-        $this->socsId = $socsId;
-        $this->apiKey = $apiKey;
+        $dotenv = Dotenv::createImmutable(dirname(__DIR__), ".env");
+        $dotenv->load();
+        $this->socsId = (int) $_ENV[$socsId];
+        $this->apiKey = (string) $_ENV[$apiKey];
+        $this->setClient();
     }
+
+    /**
+     * @return void
+     */
+    private function setClient(): void {
+        $this->client = new Client([
+            'base_uri' => 'https://www.socscms.com/socs/xml/',
+        ]);
+    }
+
+    /**
+     * @return \SimpleXMLElement|false
+     */
+    public function getClubs() {
+        $response = $this->client->request("GET", "cocurricular.ashx", [
+            'query' => [
+                'data' => 'clubs',
+                'pupils' => 1,
+                'staff' => 1,
+                'planning' => 1,
+                'ID' => $this->socsId,
+                'key' => $this->apiKey,
+            ],
+        ]);
+        $xml = simplexml_load_string($response->getBody()->getContents());
+        return $xml;
+    }
+
 
     public function echoPhrase(string $str): string
     {
         return $str;
     }
+
 }
--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace FredBradley\SOCS\Tests;
 
+use FredBradley\SOCS\SOCS;
 use PHPUnit\Framework\TestCase;
 
 class ExampleTest extends TestCase
@@ -10,5 +11,14 @@ class ExampleTest extends TestCase
     public function true_is_true()
     {
         $this->assertTrue(true);
+    }
+    /** @test */
+    public function gets_data()
+    {
+        $socs = new SOCS("SOCSID", "SOCSAPIKEY");
+        $this->assertIsObject($socs->getClubs());
+        $this->assertIsIterable($socs->getClubs());
+        $this->assertObjectHasAttribute('club', $socs->getClubs());
+        $this->assertThat($socs->getClubs(), self::isInstanceOf(\SimpleXMLElement::class));
     }
 }
