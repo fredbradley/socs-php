045855eec1d2eb0ce8de1caa6aa4d7f34d2af899
Fred Bradley <code@fredbradley.co.uk>
2021-08-07 22:01:48 +0100

    update tests

---
+++ tests/CalendarTests.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace FredBradley\SOCS\Tests;
+
+use Carbon\Carbon;
+use FredBradley\SOCS\Calendar;
+
+class CalendarTests extends BaseTest
+{
+    /** @test */
+    public function getCalendar()
+    {
+        $socs = new Calendar($this->config);
+        $result = $socs->getCalendar(Carbon::now()->subMonths(3), Carbon::now()->addMonths(3), true, true, true);
+
+        $this->assertObjectHasAttribute('CalendarEvent', $result);
+        $this->isIterableXmlObject($result);
+    }
+}
---
+++ tests/CoCurricularTests.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace FredBradley\SOCS\Tests;
+
+use Carbon\Carbon;
+use FredBradley\SOCS\CoCurricular;
+
+class CoCurricularTests extends BaseTest
+{
+    /** @test */
+    public function getClubs()
+    {
+        $socs = new CoCurricular($this->config);
+        $result = $socs->getClubs();
+        $this->isIterableXmlObject($result);
+        $this->assertObjectHasAttribute('club', $result);
+    }
+    /** @test */
+    public function getEvents()
+    {
+        $socs = new CoCurricular($this->config);
+        $result = $socs->getEvents($this->startDate, $this->endDate);
+        $this->isIterableXmlObject($result);
+        $this->assertObjectHasAttribute('event', $result);
+    }
+
+    /** @test */
+    public function getRegisters()
+    {
+        $socs = new CoCurricular($this->config);
+        $result = $socs->getRegisters(Carbon::now()->addMonths(2));
+        $this->isIterableXmlObject($result);
+        $this->assertObjectHasAttribute('pupil', $result);
+    }
+
+}
