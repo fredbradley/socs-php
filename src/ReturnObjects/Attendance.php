a2eb0c8ffa0b843da181451c839fae1dbdf4e498
Fred Bradley <code@fredbradley.co.uk>
2023-12-20 10:30:24 +0000

    add methods to attendance enum for display

--- src/ReturnObjects/Attendance.php
+++ src/ReturnObjects/Attendance.php
@@ -8,4 +8,23 @@ enum Attendance: string
     case ABSENT = 'absent';
     case AUTHORISED = 'authorised absent';
     case NOT_SET = '';
+
+    public function bootstrapClass(): string
+    {
+        return match ($this) {
+            self::ATTENDED => 'success',
+            self::ABSENT => 'danger',
+            self::AUTHORISED => 'warning',
+            default => 'secondary',
+        };
+    }
+    public function label(): string
+    {
+        return match ($this) {
+            self::ATTENDED => '<span class="label label-'.$this->bootstrapClass().'">Attended</span>',
+            self::ABSENT => '<span class="label label-'.$this->bootstrapClass().'">Absent</span>',
+            self::AUTHORISED => '<span class="label label-'.$this->bootstrapClass().'">Authorised Absent</span>',
+            default => '<span class="label label-'.$this->bootstrapClass().'">Not Set</span>',
+        };
+    }
 }
