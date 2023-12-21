cec12fd5c7fe2739aab65f741830b5926dd58f13
Fred Bradley <code@fredbradley.co.uk>
2023-12-21 07:54:11 +0000

    test the ampersand issue (caused by socs)

--- src/ReturnObjects/Event.php
+++ src/ReturnObjects/Event.php
@@ -26,10 +26,20 @@ final class Event extends ReturnObject
         foreach ($event as $key => $value) {
             if (in_array($key, ['eventid', 'clubid'])) {
                 $this->{$key} = (int) $value;
+            } elseif ($key === 'title') {
+                $this->{$key} = $this->getEventTitle($value);
             } else {
                 $this->{$key} = $value;
             }
         }
         $this->setPupilsAndStaff($event);
+    }
+
+    private function getEventTitle(string $title): string
+    {
+        $clean = (html_entity_decode($title));
+        $title = str_replace('&', '&amp;', $clean);
+
+        return trim($title);
     }
 }
